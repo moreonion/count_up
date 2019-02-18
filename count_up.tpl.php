10d15a9ef97022ecd44cb274886b1a3f0b2515f2
Roman Zimmermann <roman@more-onion.com>
2019-02-18 16:42:10 +0100

    Render count_up_title in a <h2>.

--- count_up.tpl.php
+++ count_up.tpl.php
@@ -1,2 +1,5 @@
-<?php print drupal_render($children); ?>
+<?php if (isset($children['count_up_title'])): ?>
+<h2><?php echo render($children['count_up_title']); ?></h2>
+<?php endif; ?>
+<?php echo render($children); ?>
 <div class="count-up" id="<?php echo $element['#id']; ?>"></div>
