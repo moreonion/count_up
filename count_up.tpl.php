e8cf8af1c17238cd4b9beafdaf27cf012862abb8
Katharina Zwinger <katharina@more-onion.com>
2019-07-04 17:00:02 +0200

    Display units field next to counter.

--- count_up.tpl.php
+++ count_up.tpl.php
@@ -1,5 +1,10 @@
 <?php if (isset($children['count_up_title'])): ?>
 <h2><?php echo render($children['count_up_title']); ?></h2>
 <?php endif; ?>
+<div class="count-up-wrapper">
+  <div class="count-up" id="<?php echo $element['#id']; ?>"></div>
+  <?php if (isset($children['count_up_unit'])): ?>
+  <?php echo render($children['count_up_unit']); ?>
+  <?php endif; ?>
+</div>
 <?php echo render($children); ?>
-<div class="count-up" id="<?php echo $element['#id']; ?>"></div>
